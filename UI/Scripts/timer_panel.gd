552c6bfada5be12920c1b26e64445a668401369a
Eroax <eroaxe.business@gmail.com>
2023-04-22 20:07:26 -0600

    Setup Timer Panel
    Used for displaying the current time that the run has taken in the top right of the game HUD.

---
+++ UI/Scripts/timer_panel.gd
@@ -0,0 +1,38 @@
+extends PanelContainer
+
+var cur_time := 0.0
+
+func _ready():
+	
+	%Cur_Time.text = str(cur_time).pad_decimals(3)
+	
+	cur_time = 600
+	print(Time.get_time_dict_from_unix_time(cur_time))
+	
+
+func _physics_process(delta):
+	
+	cur_time += delta
+	var time = get_time()
+#	prints(cur_time, time.sec, snapped(float(time.sec), 0.001))
+	
+	%Cur_Time.text = str("%02d:%02d:%06.3f" % [time.hour, time.min, time.sec])
+	
+
+func get_time():
+	
+	var time : Dictionary = {"sec" : 0.0, "min" : 0, "hour" : 0}
+	
+	time.sec = cur_time 
+#	print(floor(time.sec / 60))
+	
+	if time.sec >= 60:
+		time.min = time.sec / 60
+		time.sec -= floor(time.min) * 60
+	
+	if time.min >= 60:
+		time.hour = time.min / 60
+		time.min -= floor(time.hour) * 60
+	
+	return time
+	
