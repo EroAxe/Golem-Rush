8338ea25d0f8d33bd99ab29882e3df81eeb20d81
Eroax <eroaxe.business@gmail.com>
2023-04-23 12:26:38 -0600

    Implemented Score_Save.highest_score and recent_score
    Wanted some more expansive saves to make accessing the highscore and more recent score.

--- Classes/Resources/Score_Save.gd
+++ Classes/Resources/Score_Save.gd
@@ -2,10 +2,15 @@ extends Resource
 class_name Score_Save
 
 @export var scores : Array[Score]
+@export var highest_score : Score
+@export var recent_score : Score
 
 
 func save_scores(cur_scores):
 	
 	scores = cur_scores
+	recent_score = scores[-1]
+	highest_score = ScoreList.find_highest_score(cur_scores)
 	ResourceSaver.save(self, "user://Scores.res")
 	
+
