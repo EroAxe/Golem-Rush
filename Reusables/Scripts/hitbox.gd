8636adeaa258e98fa77566ee27505157fc2f3cd8
Eroax <eroaxe.business@gmail.com>
2023-04-23 12:32:04 -0600

    Added Invincibility to Hitbox
    Uses an Invincibility timer and a @export invincible_duration variable to make the hitbox invincible for a certain duration.

--- Reusables/Scripts/hitbox.gd
+++ Reusables/Scripts/hitbox.gd
@@ -1,15 +1,36 @@
 extends Area3D
+class_name Hitbox
 
 signal hitbox_hit(body)
 
 @export var weakspot := 1.0
+@export var invincible_duration := .3
+
+var invin_timer := .0
 
 func _ready():
 	
 	area_entered.connect(hit)
+	set_physics_process(false)
+	
+
+func _physics_process(delta):
+	
+	invin_timer += delta
+	if invin_timer >= invincible_duration:
+		
+		invin_timer = 0
+		set_physics_process(false)
+		
 	
 
 func hit(body):
 	
+	if body is Hitbox or invin_timer > 0:
+		
+		return
+		
+	
+	set_physics_process(true)
 	hitbox_hit.emit(body, self)
 	
