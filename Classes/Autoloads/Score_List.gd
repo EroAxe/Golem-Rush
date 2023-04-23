af463aff4acfcdfddc9d779848b197e5d6411a48
Eroax <eroaxe.business@gmail.com>
2023-04-23 12:19:27 -0600

    Added Helper functions for scores to Score_List
    A find_highest_score, a get_highscore and a add_score function to handle the respective features.

--- Classes/Autoloads/Score_List.gd
+++ Classes/Autoloads/Score_List.gd
@@ -3,9 +3,21 @@ extends Node
 var scores : Array[Score]
 @onready var cur_score := Score.new()
 
+var highscore : Score
+
 func _ready():
 	
 	load_scores()
+	
+
+func get_highscore():
+	
+	if !highscore:
+		
+		return 0
+		
+	
+	return highscore
 	
 
 func load_scores():
@@ -17,6 +29,43 @@ func load_scores():
 		return
 	
 	scores = save.scores
+	
+	highscore = find_highest_score(scores)
+	
+
+func add_score(time, dmg_taken, dps):
+	
+	var new = Score.new()
+	scores.append(new)
+	
+	new.time_to_beat = time
+	new.damage_taken = dmg_taken
+	new.dps = dps
+	
+	if highscore and new.calculate_total_score() > highscore.calculate_total_score():
+		
+		highscore = new
+		
+	
+	return new
+	
+
+func find_highest_score(scores):
+	
+	var highest : Score
+	var val := 0.0
+	for all in scores:
+		
+		all = all as Score
+		var total = all.calculate_total_score()
+		if total > val:
+			
+			val = total
+			highest = all
+			
+		
+	
+	return highest
 	
 
 func _notification(what):
